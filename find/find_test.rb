716c6ba8242a204e6d6c4be249c9fb4b5009ac37
okkun_sh <okkun.sh@gmail.com>
2019-06-04 00:45:33 +0900

    added spec find

--- find/find_test.rb
+++
@@ -1,9 +0,0 @@
-require 'minitest/autorun'
-require_relative 'find'
-
-class FindTest < Minitest::Test
-  def test_find
-    assert_equal (1..10).my_find { |i| i % 5 == 0 and i % 7 == 0 }, nil
-    assert_equal (1..100).my_find { |i| i % 5 == 0 and i % 7 == 0 }, 35
-  end
-end
---
+++ spec/find_spec.rb
@@ -0,0 +1,9 @@
+require "spec_helper"
+require_relative '../find/find'
+
+describe "#find" do
+  it do
+    expect((1..10).my_find { |i| i % 5 == 0 and i % 7 == 0 }).to eq nil
+    expect((1..100).my_find { |i| i % 5 == 0 and i % 7 == 0 }).to eq 35
+  end
+end
